b465dbb0c00feb667cddf4c9ed988c124183a3f2
Laure <148218914+laureemrt@users.noreply.github.com>
2024-04-02 08:14:46 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,52 @@
+import gymnasium as gym
+
+env = gym.make("highway-fast-v0", render_mode="rgb_array")
+
+config = {
+    "observation": {
+        "type": "OccupancyGrid",
+        "vehicles_count": 10,
+        "features": ["presence", "x", "y", "vx", "vy", "cos_h", "sin_h"],
+        "features_range": {
+            "x": [-100, 100],
+            "y": [-100, 100],
+            "vx": [-20, 20],
+            "vy": [-20, 20],
+        },
+        "grid_size": [[-20, 20], [-20, 20]],
+        "grid_step": [5, 5],
+        "absolute": False,
+    },
+    "action": {
+        "type": "DiscreteAction",
+    },
+    "lanes_count": 3,
+    "vehicles_count": 10,
+    "duration": 20,  # [s]
+    "initial_spacing": 0,
+    "collision_reward": -1,  # The reward received when colliding with a vehicle.
+    "right_lane_reward": 0.5,  # The reward received when driving on the right-most lanes, linearly mapped to
+    # zero for other lanes.
+    "high_speed_reward": 0.1,  # The reward received when driving at full speed, linearly mapped to zero for
+    # lower speeds according to config["reward_speed_range"].
+    "lane_change_reward": 0,
+    "reward_speed_range": [
+        20,
+        30,
+    ],  # [m/s] The reward for high speed is mapped linearly from this range to [0, HighwayEnv.HIGH_SPEED_REWARD].
+    "simulation_frequency": 5,  # [Hz]
+    "policy_frequency": 1,  # [Hz]
+    "other_vehicles_type": "highway_env.vehicle.behavior.IDMVehicle",
+    "screen_width": 600,  # [px]
+    "screen_height": 150,  # [px]
+    "centering_position": [0.3, 0.5],
+    "scaling": 5.5,
+    "show_trajectories": True,
+    "render_agent": True,
+    "offscreen_rendering": False,
+    "disable_collision_checks": True,
+}
+
+
+env.unwrapped.configure(config)
+print(env.reset())
